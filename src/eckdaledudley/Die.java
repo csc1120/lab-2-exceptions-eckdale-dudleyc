0490fe638f0336d31b0297cdcc505429ebc8dc99
Chloe Eckdale-Dudley <eckdale-dudleyc@msoe.edu>
2024-09-10 10:48:14 -0500

    COMMIT 1

---
+++ src/eckdaledudley/Die.java
@@ -0,0 +1,41 @@
+/*
+ * Course: CSC1020
+ * Lab 2 - Exceptions
+ * Die class
+ * Name: Chloe Eckdale-Dudley
+ * Last Updated: 9/10/2024
+ */
+package eckdaledudley;
+
+import java.util.Random;
+
+public class Die {
+
+    public static final int MIN_SIDES = 2;
+    public static final int MAX_SIDES = 100;
+    private int currentValue;
+    private final int numSides;
+    private final Random random = new Random();
+
+    public Die(int numSides) {
+        this.numSides = numSides;
+        if(this.numSides > MAX_SIDES || this.numSides < MIN_SIDES) {
+            throw new IllegalArgumentException("Out of range");
+        }
+    }
+
+    public int getCurrentValue() {
+        int sideUp = currentValue;
+        currentValue = 0;
+
+        if(sideUp < 1 || sideUp > numSides) {
+            throw new DieNotRolledException("Die not rolled");
+        }
+        return sideUp;
+
+    }
+    public void roll() {
+        currentValue = random.nextInt(1,numSides+1);
+    }
+
+}
---
+++ src/eckdaledudley/DieNotRolledException.java
@@ -0,0 +1,16 @@
+/*
+ * Course: CSC1020
+ * Lab 2 - Exceptions
+ * DieNotRolledException class
+ * Name: Chloe Eckdale-Dudley
+ * Last Updated: 9/10/2024
+ */
+package eckdaledudley;
+
+public class DieNotRolledException extends RuntimeException {
+
+    public DieNotRolledException(String message) {
+        super(message);
+    }
+
+}
